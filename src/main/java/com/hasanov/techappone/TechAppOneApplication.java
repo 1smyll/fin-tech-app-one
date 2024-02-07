d5a5002bb2c52b0f89d1859c4e0e3d8513d6dd50
1smyll <ismylhsnvv@gmail.com>
2024-02-07 14:13:25 +0400

    first commit

---
+++ src/main/java/com/hasanov/techappone/TechAppOneApplication.java
@@ -0,0 +1,13 @@
+package com.hasanov.techappone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechAppOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechAppOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hasanov/techappone/TechAppOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasanov.techappone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechAppOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
